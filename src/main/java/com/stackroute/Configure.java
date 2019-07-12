1b59340dff1f0d5e3ffb845e5145a3f2ff277e45
raviasmitha <asmithachowdary1511@gmail.com>
2019-07-12 15:35:56 +0530

    task2

--- src/main/java/com/stackroute/Configure.java
+++ src/main/java/com/stackroute/Configure.java
@@ -5,6 +5,7 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Configure {
@@ -18,8 +19,15 @@ public class Configure {
     return actor;
   }
 
-  @Bean(name = "movie")
+  @Bean(name = {"movie1","movie"})
+  @Scope("singleton")
   public Movie movieBean(){
+    return new Movie(actorBean());
+  }
+
+  @Bean(name = "movie2")
+  @Scope("prototype")
+  public Movie moviebean2(){
     return new Movie(actorBean());
   }
 }
